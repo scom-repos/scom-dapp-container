f87f10e4602e45ee8a06b8f7ee2660981c7b8fc7
celina-ijs <celina.dev@instajs.com>
2023-02-17 14:38:42 +0700

    Add theme settings

--- modules/main/footer.tsx
+++ modules/main/footer.tsx
@@ -4,7 +4,9 @@ import {
   Label,
   ControlElement,
   Styles,
-  application
+  application,
+  HStack,
+  Control
 } from '@ijstech/components';
 import Assets from '@pageblock-dapp-container/assets';
 
@@ -20,40 +22,42 @@ const Theme = Styles.Theme.ThemeVars;
 
 @customElements('dapp-container-footer')
 export class DappContainerFooter extends Module {
-  private _footer: string;
-  private lblFooter: Label;
+  private _footer: Control;
+  private lblFooter: HStack;
 
   constructor(parent?: any) {
     super(parent);
   }
 
-  async init() {
+  init() {
     super.init();
-    this.footer = "Powered By SECURE COMPUTE";
   }
 
   get footer() {
     return this._footer;
   }
 
-  set footer(value: string) {
+  set footer(value: Control) {
     this._footer = value;
-    this.lblFooter.caption = value;
+    this.lblFooter.clearInnerHTML();
+    this.lblFooter.appendChild(value);
   }
 
   render() {
     return (
       <i-hstack
         class="footer"
-        height={50}
         horizontalAlignment="start"
         verticalAlignment="center"
-        padding={{ left: 20, right: 20, top: 10, bottom: 10 }}
-        border={{ width: 1, style: 'solid', color: Theme.divider }}
-        gap={10}
+        padding={{ left: '5.25rem', right: '5.25rem', bottom: '1.25rem' }}
+        // border={{ width: 1, style: 'solid', color: Theme.divider }}
       >
-        <i-image height={30} width={30} url={Assets.logo}></i-image>
-        <i-label id="lblFooter" font={{ color: Theme.text.primary }}></i-label>
+        {/* <i-image height={30} width={30} url={Assets.logo}></i-image> */}
+        <i-hstack id="lblFooter" gap={4} verticalAlignment="center">
+          <i-label caption="Powered By" font={{size: '0.75rem', color: '#000'}}></i-label>
+          <i-label caption="SECURE" font={{size: '0.875rem', color: '#F99E43', weight: 700}}></i-label>
+          <i-label caption="COMPUTE" font={{size: '0.875rem', color: '#000', weight: 700}}></i-label>
+        </i-hstack>
       </i-hstack>
     );
   }
